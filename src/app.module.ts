8d94cd37284a7777bf91293b45805c35c89aff70
Daniel Tršťanský <daniel.trstansky@gmail.com>
2025-12-01 08:42:08 +0100

    edit: import auth and documents modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+import { DocumentsModule } from './documents/documents.module';
+// v prod samozrejme aj toto musim zmenit na dotenv
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -17,6 +21,8 @@ import { UsersModule } from './users/users.module';
       synchronize: true,
     }),
     UsersModule,
+    AuthModule,
+    DocumentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
